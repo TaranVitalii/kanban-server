8f0ba3764f602567f2c375d9e439432ec2edf552
ikn0wQQ <kn0wnOutCast@gmail.com>
2019-07-12 18:05:57 +0300

    with dotenv

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const app = require('./app.js');
 const mongoose = require('mongoose');
+require('dotenv').config()
 
-mongoose.connect('mongodb://localhost/kanban', {useNewUrlParser: true});
+// const config = require('config'); //мне понравилось через dotenv добавлять переменные окружение а с config что-то не сложилось
+// const host = config.get('DataBase.host');
+
+const host = process.env.DB_CONN;
+const port = process.env.PORT;
+console.log(port)
+mongoose.connect(process.env.DB_CONN, {useNewUrlParser: true});
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
@@ -9,4 +16,4 @@ db.once('open', function() {
   console.log('we\'re connected!')
 });
 
-app.listen(8089, ()=> console.log('Сервер работает - localhost:8089'));
+app.listen(port, ()=> console.log('Сервер работает - localhost:8089'));
